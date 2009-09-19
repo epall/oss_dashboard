99fa619961086a7217b9cc8ce92b423ea9a9822c
Eric Allen <ericpallen@gmail.com>
2009-09-19 12:19:57 -0400

    Repository age detection for github and Google Code.

--- rcos_monitor.rb
+++ rcos_monitor.rb
@@ -9,12 +9,17 @@ require 'open-uri'
 require 'feed_detector'
 
 SECONDS_IN_DAY = 60 * 60 * 24
+BLOG_STALE_AGE = 14
+CODE_STALE_AGE = 14
 
 helpers do
-    def render_column(col_name, value)
+    def render_column(col_name, project)
+        value = project[col_name]
         return 'No' if value.nil?
         if col_name == 'Blog'
             "<a href=\"#{value}\">Yes</a> (#{last_update(value)})"
+        elsif col_name == 'Source Code'
+            render_source_code(project)
         elsif value.is_a? String and value.match(/http/)
             "<a href=\"#{value}\">Yes</a>"
         elsif value.is_a? Array
@@ -24,13 +29,35 @@ helpers do
         end
     end
 
-    def value_style(col_name, value)
+    def repo_update(repo_info)
+        last_update(repo_info['URL']) if ['github', 'Google Code'].include? repo_info['Type']
+    end
+
+    def repo_age(repo_info)
+        case(repo_info['Type'])
+        when 'github' then blog_age(repo_info['URL'])
+        when 'Google Code' then blog_age(repo_info['URL'])
+        else raise "Repository type not supported: #{repo_info['Type']}"
+        end
+    end
+
+    def render_source_code(project)
+        return "<a href=\"#{project['Source Code']}\">Yes</a>" unless project['Repo']
+        if ['github', 'Google Code'].include? project['Repo']['Type']
+            "<a href=\"#{project['Source Code']}\">#{project['Repo']['Type']}</a> (#{repo_update(project['Repo'])})"
+        else "<a href=\"#{project['Source Code']}\">Yes</a>"
+        end
+    end
+    
+    def value_style(col_name, project)
+        value = project[col_name]
         unless ['Name', 'Contributors', 'Website'].include? col_name
             return 'no' if value.nil?
-
             if col_name == 'Blog'
-                if blog_age(value) > 7
-                    return 'stale'
+                return blog_age(value) > BLOG_STALE_AGE ? 'stale' : 'yes'
+            elsif col_name == 'Source Code'
+                if project['Repo']
+                    return repo_age(project['Repo']) > CODE_STALE_AGE ? 'stale' : 'yes'
                 else
                     return 'yes'
                 end
@@ -53,12 +80,15 @@ helpers do
     end
 
     def last_update(blog_url)
-        fetch_blog(blog_url).entries.first.published.strftime('%m/%d')
+        published = fetch_blog(blog_url).entries.first.published
+        published ||= fetch_blog(blog_url).entries.first.updated 
+        published.strftime('%m/%d')
     end
 
     def blog_age(blog_url)
-        rss = fetch_blog(blog_url)
-        age = Time.now - rss.entries.first.published
+        published = fetch_blog(blog_url).entries.first.published
+        published ||= fetch_blog(blog_url).entries.first.updated 
+        age = Time.now - published
         days_old = (age / SECONDS_IN_DAY)
         return days_old
     end
