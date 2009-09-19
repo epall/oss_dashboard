91b37f44a02c5e05af5c8e689abaf424e81d49b0
Eric Allen <ericpallen@gmail.com>
2009-09-19 11:27:57 -0400

    Up and running with simple static behavior.

--- rcos_monitor.rb
+++ rcos_monitor.rb
@@ -4,7 +4,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+helpers do
+    def render_column(col_name, value)
+        return 'No' if value.nil?
+        if value.is_a? String and value.match(/http/)
+            "<a href=\"#{value}\">Yes</a>"
+        elsif value.is_a? Array
+            value.join('\n<br>\n')
+        else
+            value
+        end
+    end
+
+    def value_style(col_name, value)
+        unless ['Name', 'Contributors', 'Website'].include? col_name
+            return 'no' if value.nil?
+            return 'yes'
+        end
+        ''
+    end
+end
 
 get '/' do
+    @columns = ['Name', 'Contributors', 'Website', 'Blog', 'Wiki', 'Source Code']
+    @projects = YAML.load(File.open('projects.yml'))
     erb :index
 end
