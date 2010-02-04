09ea4b170d480f43cf227a3414cf2920e64d76cb
NateStedman <natesm@gmail.com>
2010-02-03 23:28:26 -0500

    Use the default view for blog posts.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-  layout 'simple'
   def index
     @events = Event.blog.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
     respond_to do |format|
