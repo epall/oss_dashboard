aeb13f267c2e690b0ea6834d7b8fbc35b151b584
NateStedman <natesm@gmail.com>
2010-02-04 03:44:24 -0500

    More CSS improvements, improvements to blog view: 'st', 'nd' 'rd', 'th' on dates.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+	def day(date)
+		day = date.strftime('%d')
+		day.slice!('0') if (date.day < 10)
+		if (date.day == 1 or date.day == 21 or date.day == 31)
+			day += 'st'
+		elsif (date.day == 2 or date.day == 22)
+			day += 'nd'
+		elsif (date.day == 3 or date.day == 23)
+			day += 'rd'
+		else
+			day += 'th'
+		end
+		return day
+	end
 end
