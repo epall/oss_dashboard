7a2fe2cd011e9a59bf24eaea8b0c5b8c843afb0f
Eric Allen <ericpallen@gmail.com>
2010-02-19 13:23:51 -0500

    Friendlier, more informative weekly email

--- app/models/bugger.rb
+++ app/models/bugger.rb
@@ -8,16 +8,11 @@ class Bugger < ActionMailer::Base
       current.activity_this_week > best.activity_this_week ? current : best
     end
     
-    least_active = group.projects.inject do |best, current|
-      current.activity_this_week < best.activity_this_week ? current : best
-    end
-    
     sorted_by_activity = group.projects.sort_by(&:total_activity)
-    
     top_active = sorted_by_activity.reverse[0..2]
-    bottom_active = sorted_by_activity[0..2]
     
-    body :group => group, :most_active => most_active, :least_active => least_active, :top_active => top_active,
-      :bottom_active => bottom_active
+    blog_posts = Event.blog.find(:all, :conditions => ['created_at > ?', Time.now-7.days])
+    
+    body :group => group, :most_active => most_active, :top_active => top_active, :blogs => blog_posts
   end
 end
