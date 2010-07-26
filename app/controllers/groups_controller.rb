a77fd0cb2d4e2890ba2a851f5f6e6521fc145f1f
Eric Allen <ericpallen@gmail.com>
2010-07-26 10:14:48 -0400

    Don't crash on null contributors

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -34,7 +34,7 @@ class GroupsController < ApplicationController
     @stats['none'] = @projects.reject{|p| p.blog || p.wiki || p.source_code}.size
     @stats['all_three'] = @projects.select{|p| p.blog && p.wiki && p.source_code}.size
     @stats['last_week'] = @projects.select{|p| [p.blog_age, p.source_code_age].min < 7}.size
-    @stats['members'] =  @projects.map(&:contributors).map{|c| c.split(/, ?/)}.flatten.uniq.size
+    @stats['members'] =  @projects.map(&:contributors).map{|c| (c || "").split(/, ?/)}.flatten.uniq.size
   end
 
   def admin
